2400c6797814df4dba42d7faa397ec74e1de73bb
Levin Jian <jianzhirong@gmail.com>
2020-12-11 16:59:09 +0800

    ekf fusion for lidar detection, 0.31

--- student/filter.py
+++ student/filter.py
@@ -30,8 +30,14 @@ class Filter:
         ############
         # TODO Step 1: implement and return system matrix F
         ############
+        dt = params.dt
+        n = params.dim_state
+        F = np.identity(params.dim_state).reshape(n,n)
+        F[0, 3] = dt 
+        F[1, 4] = dt 
+        F[2, 5] = dt 
 
-        return 0
+        return np.matrix(F)
         
         ############
         # END student code
@@ -41,8 +47,11 @@ class Filter:
         ############
         # TODO Step 1: implement and return process noise covariance Q
         ############
+        dt = params.dt
+        Q = np.zeros((params.dim_state, params.dim_state))
+        np.fill_diagonal(Q, dt* params.q)
 
-        return 0
+        return np.matrix(Q)
         
         ############
         # END student code
@@ -52,8 +61,14 @@ class Filter:
         ############
         # TODO Step 1: predict state x and estimation error covariance P to next timestep, save x and P in track
         ############
-
-        pass
+        F = self.F()
+        Q = self.Q()
+        x = F * track.x 
+        P = F * track.P * F.T + Q
+        
+        track.set_x(x)
+        track.set_P(P)
+        
         
         ############
         # END student code
@@ -64,6 +79,22 @@ class Filter:
         # TODO Step 1: update state x and covariance P with associated measurement, save x and P in track
         ############
         
+        x = track.x
+        P = track.P 
+        
+        y = self.gamma(track, meas)
+        H = meas.sensor.get_H(x)
+        S = self.S(track, meas, H)
+        
+        K = P * H.T * S.I 
+        I = np.identity(params.dim_state)
+        
+        x = x + K*y
+        P = (I - K * H) * P
+        
+        track.set_x(x)
+        track.set_P(P)
+        
         ############
         # END student code
         ############ 
@@ -73,8 +104,11 @@ class Filter:
         ############
         # TODO Step 1: calculate and return residual gamma
         ############
+        z = meas.z
+        z_pred = meas.sensor.get_hx(track.x)
+        y = z - z_pred
 
-        return 0
+        return y
         
         ############
         # END student code
@@ -84,8 +118,11 @@ class Filter:
         ############
         # TODO Step 1: calculate and return covariance of residual S
         ############
+        
+        S = H * track.P * H.T + meas.R
+        
 
-        return 0
+        return S
         
         ############
         # END student code
