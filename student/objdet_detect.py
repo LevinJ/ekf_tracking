30d56fee8c011bbaa201305bd90dabcb291b6b01
Levin Jian <jianzhirong@gmail.com>
2020-12-22 13:56:31 +0800

    bev detection

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -25,6 +25,7 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 # model-related
 from tools.objdet_models.resnet.models import fpn_resnet
 from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_processing 
+from tools.objdet_models.resnet.utils.torch_utils import _sigmoid
 
 from tools.objdet_models.darknet.models.darknet2pytorch import Darknet as darknet
 from tools.objdet_models.darknet.utils.evaluation_utils import post_processing_v2
@@ -62,6 +63,18 @@ def load_configs_model(model_name='darknet', configs=None):
         ####### ID_S3_EX1-3 START #######     
         #######
         print("student task ID_S3_EX1-3")
+        extra_configs = {'saved_fn': 'fpn_resnet_18', 'arch': 'fpn_resnet_18', 'pretrained_path': '../checkpoints/fpn_resnet_18/fpn_resnet_18_epoch_300.pth', 
+                         'K': 50, 'no_cuda': False, 'gpu_idx': 0, 'num_samples': None, 'num_workers': 1, 'batch_size': 1, 'peak_thresh': 0.2, 
+                         'save_test_output': False, 'output_format': 'image', 'output_video_fn': 'out_fpn_resnet_18', 'output_width': 608, 
+                         'pin_memory': True, 'distributed': False, 'input_size': [608, 608], 'hm_size': [152, 152], 'down_ratio': 4, 'max_objects': 50, 
+                         'imagenet_pretrained': False, 'head_conv': 64, 'num_classes': 3, 'num_center_offset': 2, 'num_z': 1, 'num_dim': 3, 'num_direction': 2, 
+                         'heads': {'hm_cen': 3, 'cen_offset': 2, 'direction': 2, 'z_coor': 1, 'dim': 3}, 
+                         'num_input_features': 4, 'root_dir': '../', 'dataset_dir': '../dataset/kitti'}
+        for k,v in extra_configs.items():
+            configs[k] = v
+        configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'resnet')
+        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'fpn_resnet_18_epoch_300.pth')
+        
 
         #######
         ####### ID_S3_EX1-3 END #######     
@@ -119,6 +132,10 @@ def create_model(configs):
         ####### ID_S3_EX1-4 START #######     
         #######
         print("student task ID_S3_EX1-4")
+        arch_parts = configs.arch.split('_')
+        num_layers = int(arch_parts[-1])
+        model = fpn_resnet.get_pose_net(num_layers=num_layers, heads=configs.heads, head_conv=configs.head_conv,
+                                        imagenet_pretrained=configs.imagenet_pretrained)
 
         #######
         ####### ID_S3_EX1-4 END #######     
@@ -168,6 +185,18 @@ def detect_objects(input_bev_maps, model, configs):
             ####### ID_S3_EX1-5 START #######     
             #######
             print("student task ID_S3_EX1-5")
+            outputs['hm_cen'] = _sigmoid(outputs['hm_cen'])
+            outputs['cen_offset'] = _sigmoid(outputs['cen_offset'])
+            # detections size (batch_size, K, 10)
+            detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'],
+                                outputs['dim'], K=configs.K)
+            detections = detections.cpu().numpy().astype(np.float32)
+            detections = post_processing(detections, configs)
+            detections = detections[0][1]
+                
+            
+            
+                
 
             #######
             ####### ID_S3_EX1-5 END #######     
@@ -190,6 +219,18 @@ def detect_objects(input_bev_maps, model, configs):
         
     #######
     ####### ID_S3_EX2 START #######   
+    for obj in detections:
+        id, bev_x, bev_y,z, h,bev_w,bev_l,yaw = obj
+        
+        x = bev_y/configs.bev_height * (configs.lim_x[1] - configs.lim_x[0])
+        y = bev_x/configs.bev_width * (configs.lim_y[1] - configs.lim_y[0]) - (configs.lim_y[1] - configs.lim_y[0])/2.0
+        w = bev_w / configs.bev_width  * (configs.lim_y[1] - configs.lim_y[0])
+        l = bev_l /configs.bev_height * (configs.lim_x[1] - configs.lim_x[0])
+        if ((x >= configs.lim_x[0]) and (x <= configs.lim_x[1]) 
+            and (y >= configs.lim_y[0]) and (y <= configs.lim_y[1]) 
+            and (z >= configs.lim_z[0]) and (z <= configs.lim_z[1])):
+            objects.append([1, x,y,z,h,w,l,yaw])
+        
     
     return objects    
 
